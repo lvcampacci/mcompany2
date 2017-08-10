f764020a736df6b5fb42c41850cfacc059a7ba8f
lvcampacci <luciano.campacci@outlook.com>
2017-08-10 01:06:50 -0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <?php
 
-// Passando os dados obtidos pelo formuláo para as variáis abaixo
+// Passando os dados obtidos pelo formulÃ¡o para as variÃ¡is abaixo
 $quebra_linha = "\n";
 $emailsender='gerencia@mcompanymt.com.br';
 $nomeremetente     = "Formulario do Site";
@@ -11,23 +11,24 @@ $mensagem          = "Texto do Conteudo";
 $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
+$subject = $_POST['subject'];
 
 /* Montando a mensagem a ser enviada no corpo do e-mail. */
-$mensagemHTML = $name.' '.$email.' '.$message;
+$mensagemHTML = $name.' '.$email.' '.$subject.' '.$message;
 
 
-/* Montando o cabeçho da mensagem */
+/* Montando o cabeÃ§ho da mensagem */
 $headers = "MIME-Version: 1.1".$quebra_linha;
 $headers .= "Content-type: text/html; charset=iso-8859-1".$quebra_linha;
-// Perceba que a linha acima conté"text/html", sem essa linha, a mensagem nãchegaráormatada.
+// Perceba que a linha acima contÃ©"text/html", sem essa linha, a mensagem nÃ£chegarÃ¡ormatada.
 $headers .= "From: ".$emailsender.$quebra_linha;
 $headers .= "Return-Path: " . $emailsender . $quebra_linha;
 $headers .= "Reply-To: ".$emailremetente.$quebra_linha;
-// Note que o e-mail do remetente serásado no campo Reply-To (Responder Para)
+// Note que o e-mail do remetente serÃ¡sado no campo Reply-To (Responder Para)
 
 /* Enviando a mensagem */
 mail($emaildestinatario, $assunto, $mensagemHTML, $headers, "-r". $emailsender);
 
 
-header("Location: http://www.mcompany.com.br");
+header("Location: http://www.mcompanymt.com.br");
 ?>
